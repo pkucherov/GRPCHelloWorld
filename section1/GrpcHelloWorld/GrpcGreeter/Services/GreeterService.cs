1d4b2741f1bf23d85109c47d3fb75ba21309dcdd
Pavel Kucherov <26567338+pkucherov@users.noreply.github.com>
2020-11-11 15:25:03 +0100

    Change for c++  client

--- section1/GrpcHelloWorld/GrpcGreeter/Services/GreeterService.cs
+++ section1/GrpcHelloWorld/GrpcGreeter/Services/GreeterService.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
-
+using Helloworld;
 namespace GrpcGreeter
 {
-    public class GreeterService : Greeter.GreeterBase
+    public class GreeterService : Helloworld.Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
         public GreeterService(ILogger<GreeterService> logger)
@@ -15,7 +15,7 @@ namespace GrpcGreeter
             _logger = logger;
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        public override Task<Helloworld.HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
             {
--- section1/GrpcHelloWorld/GrpcGreeterClient/Program.cs
+++ section1/GrpcHelloWorld/GrpcGreeterClient/Program.cs
@@ -6,13 +6,27 @@ namespace GrpcGreeterClient
 {
     class Program
     {
-        static async Task Main(string[] args)
+        //static async Task Main(string[] args)
+        //{
+        //    // The port number(5001) must match the port of the gRPC server.
+        //    //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+        //    using var channel = GrpcChannel.ForAddress("http://127.0.0.1:50051");
+        //    var client = new Greeter.GreeterClient(channel);
+        //    var reply = await client.SayHelloAsync(
+        //                      new HelloRequest { Name = "GreeterClient" });
+        //    Console.WriteLine("Greeting: " + reply.Message);
+        //    Console.WriteLine("Press any key to exit...");
+        //    Console.ReadKey();
+        //}
+
+        static void Main(string[] args)
         {
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient" });
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            using var channel = GrpcChannel.ForAddress("http://127.0.0.1:50051");
+            var client = new Helloworld.Greeter.GreeterClient(channel);
+            var reply = client.SayHello(
+                              new Helloworld.HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
